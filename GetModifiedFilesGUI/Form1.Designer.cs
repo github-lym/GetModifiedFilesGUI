c2efba81864485846d13cbbebf6a876f39e990ff
github-lym <allregister@gmail.com>
2022-03-12 16:28:22 +0800

    Init Commit

---
+++ GetModifiedFilesGUI/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace GetModifiedFilesGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox_Option = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker_Date = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker_Time = new System.Windows.Forms.DateTimePicker();
+            this.folderBrowserDialog_Source = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_SourcePath = new System.Windows.Forms.TextBox();
+            this.button_Source = new System.Windows.Forms.Button();
+            this.button_Destination = new System.Windows.Forms.Button();
+            this.textBox_DestinationPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog_Destination = new System.Windows.Forms.FolderBrowserDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_ExcludeFolder = new System.Windows.Forms.TextBox();
+            this.textBox_ExcludeExtension = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button_Execute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox_Option
+            // 
+            this.comboBox_Option.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox_Option.FormattingEnabled = true;
+            this.comboBox_Option.Location = new System.Drawing.Point(152, 14);
+            this.comboBox_Option.Name = "comboBox_Option";
+            this.comboBox_Option.Size = new System.Drawing.Size(306, 37);
+            this.comboBox_Option.TabIndex = 0;
+            this.comboBox_Option.SelectedIndexChanged += new System.EventHandler(this.comboBox_Option_Change);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "選擇情境";
+            // 
+            // dateTimePicker_Date
+            // 
+            this.dateTimePicker_Date.CalendarFont = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker_Date.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker_Date.Location = new System.Drawing.Point(18, 276);
+            this.dateTimePicker_Date.Name = "dateTimePicker_Date";
+            this.dateTimePicker_Date.Size = new System.Drawing.Size(188, 31);
+            this.dateTimePicker_Date.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "取出此時間後的檔案";
+            // 
+            // dateTimePicker_Time
+            // 
+            this.dateTimePicker_Time.CalendarFont = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker_Time.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dateTimePicker_Time.Location = new System.Drawing.Point(224, 276);
+            this.dateTimePicker_Time.Name = "dateTimePicker_Time";
+            this.dateTimePicker_Time.Size = new System.Drawing.Size(188, 31);
+            this.dateTimePicker_Time.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 35);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "選擇來源路徑";
+            // 
+            // textBox_SourcePath
+            // 
+            this.textBox_SourcePath.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_SourcePath.Location = new System.Drawing.Point(18, 106);
+            this.textBox_SourcePath.Name = "textBox_SourcePath";
+            this.textBox_SourcePath.Size = new System.Drawing.Size(564, 31);
+            this.textBox_SourcePath.TabIndex = 6;
+            // 
+            // button_Source
+            // 
+            this.button_Source.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Source.Location = new System.Drawing.Point(213, 64);
+            this.button_Source.Name = "button_Source";
+            this.button_Source.Size = new System.Drawing.Size(101, 36);
+            this.button_Source.TabIndex = 7;
+            this.button_Source.Text = "瀏覽";
+            this.button_Source.UseVisualStyleBackColor = true;
+            this.button_Source.Click += new System.EventHandler(this.button_Source_Click);
+            // 
+            // button_Destination
+            // 
+            this.button_Destination.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Destination.Location = new System.Drawing.Point(213, 155);
+            this.button_Destination.Name = "button_Destination";
+            this.button_Destination.Size = new System.Drawing.Size(101, 36);
+            this.button_Destination.TabIndex = 10;
+            this.button_Destination.Text = "瀏覽";
+            this.button_Destination.UseVisualStyleBackColor = true;
+            this.button_Destination.Click += new System.EventHandler(this.button_Destination_Click);
+            // 
+            // textBox_DestinationPath
+            // 
+            this.textBox_DestinationPath.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_DestinationPath.Location = new System.Drawing.Point(18, 197);
+            this.textBox_DestinationPath.Name = "textBox_DestinationPath";
+            this.textBox_DestinationPath.Size = new System.Drawing.Size(564, 31);
+            this.textBox_DestinationPath.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 35);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "選擇目的路徑";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 35);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "排除的資料夾";
+            // 
+            // textBox_ExcludeFolder
+            // 
+            this.textBox_ExcludeFolder.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_ExcludeFolder.Location = new System.Drawing.Point(19, 368);
+            this.textBox_ExcludeFolder.Name = "textBox_ExcludeFolder";
+            this.textBox_ExcludeFolder.Size = new System.Drawing.Size(564, 31);
+            this.textBox_ExcludeFolder.TabIndex = 12;
+            // 
+            // textBox_ExcludeExtension
+            // 
+            this.textBox_ExcludeExtension.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_ExcludeExtension.Location = new System.Drawing.Point(19, 458);
+            this.textBox_ExcludeExtension.Name = "textBox_ExcludeExtension";
+            this.textBox_ExcludeExtension.Size = new System.Drawing.Size(564, 31);
+            this.textBox_ExcludeExtension.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 420);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 35);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "排除的副檔名";
+            // 
+            // button_Execute
+            // 
+            this.button_Execute.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_Execute.Location = new System.Drawing.Point(481, 15);
+            this.button_Execute.Name = "button_Execute";
+            this.button_Execute.Size = new System.Drawing.Size(101, 36);
+            this.button_Execute.TabIndex = 15;
+            this.button_Execute.Text = "執行";
+            this.button_Execute.UseVisualStyleBackColor = true;
+            this.button_Execute.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 499);
+            this.Controls.Add(this.button_Execute);
+            this.Controls.Add(this.textBox_ExcludeExtension);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_ExcludeFolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button_Destination);
+            this.Controls.Add(this.textBox_DestinationPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_Source);
+            this.Controls.Add(this.textBox_SourcePath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker_Time);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker_Date);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_Option);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GetModifiedFilesGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Option;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Date;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Time;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Source;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_SourcePath;
+        private System.Windows.Forms.Button button_Source;
+        private System.Windows.Forms.Button button_Destination;
+        private System.Windows.Forms.TextBox textBox_DestinationPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Destination;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_ExcludeFolder;
+        private System.Windows.Forms.TextBox textBox_ExcludeExtension;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button_Execute;
+    }
+}
+
---
+++ GetModifiedFilesGUI/Form1.cs
@@ -0,0 +1,212 @@
+﻿using IniFiles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetModifiedFilesGUI
+{
+    public partial class Form1 : Form
+    {
+        IniFile ini = new IniFile("settings.ini");
+        string noData = "j5W!";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox_Option.Items.Add("無");
+            comboBox_Option.SelectedItem = "無";
+
+            //取得已有情境
+            string ini_opt = ini.ReadString("Option", "Value");
+            string[] opts = ini_opt.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            if (opts.Length > 0)
+                foreach (var item in opts)
+                    comboBox_Option.Items.Add(item);
+
+            //dateTimePicker_Date.ShowUpDown = true;
+            dateTimePicker_Time.Format = DateTimePickerFormat.Time;
+            dateTimePicker_Time.ShowUpDown = true;
+        }
+
+        #region 選取路徑
+        private void button_Source_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog_Source.ShowDialog() == DialogResult.OK)
+            {
+                textBox_SourcePath.Text = folderBrowserDialog_Source.SelectedPath;
+            }
+        }
+
+        private void button_Destination_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog_Destination.ShowDialog() == DialogResult.OK)
+            {
+                textBox_DestinationPath.Text = folderBrowserDialog_Destination.SelectedPath;
+            }
+        }
+        #endregion
+
+        #region 重新設定介面
+        private void ResetForm()
+        {
+            comboBox_Option.SelectedItem = "無";
+            textBox_SourcePath.Text = string.Empty;
+            textBox_DestinationPath.Text = string.Empty;
+            textBox_ExcludeFolder.Text = string.Empty;
+            textBox_ExcludeExtension.Text = string.Empty;
+            dateTimePicker_Date.Value = DateTime.Now;
+            dateTimePicker_Time.Value = DateTime.Now;
+        }
+        #endregion
+
+        #region 情境下拉選單變更 (comboBox_Option)
+        private void comboBox_Option_Change(object sender, EventArgs e)
+        {
+            string opt_string = comboBox_Option.Text;
+            if (string.IsNullOrWhiteSpace(opt_string) || "無".Equals(opt_string))
+                ResetForm();
+            else
+            {
+                string[] opt_keys = ini.ReadKeyNames(opt_string);
+                string[] opt_values = ini.ReadSectionValue(opt_string);
+                Dictionary<string, string> opt_KeyValues = new Dictionary<string, string>();
+                for (int i = 0; i < opt_keys.Length; i++)
+                    opt_KeyValues.Add(opt_keys[i], opt_values[i]);
+
+                if (opt_KeyValues.Keys.Contains("source_path"))
+                    textBox_SourcePath.Text = opt_KeyValues["source_path"];
+                if (opt_KeyValues.Keys.Contains("source_path"))
+                    textBox_DestinationPath.Text = opt_KeyValues["destination_path"];
+                if (opt_KeyValues.Keys.Contains("source_path"))
+                    textBox_ExcludeFolder.Text = opt_KeyValues["exclude_folder"];
+                if (opt_KeyValues.Keys.Contains("source_path"))
+                    textBox_ExcludeExtension.Text = opt_KeyValues["exclude_extension"];
+
+                string ddd = string.Empty;
+            }
+        }
+        #endregion
+
+        private void button_Execute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //取得來源目的資料夾
+                string fromPath = textBox_SourcePath.Text.Trim();
+                string toPath = textBox_DestinationPath.Text.Trim();
+                if(string.IsNullOrWhiteSpace(fromPath) || string.IsNullOrWhiteSpace(toPath))
+                {
+                    MessageBox.Show("Err:來源目標路徑不得為空");
+                    return;
+                }
+                if (fromPath.Equals(toPath))
+                {
+                    MessageBox.Show("Err:來源目標路徑不得相同");
+                    return;
+                }
+
+                //取得排除資料夾
+                string exDir = textBox_ExcludeFolder.Text.Trim();
+                string[] exDirArray;
+                if (!string.IsNullOrWhiteSpace(exDir))
+                    exDirArray = exDir.ToLower().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                else
+                    exDirArray = new string[1] { noData };
+
+                //取得排除副檔名
+                string exFiles = textBox_ExcludeExtension.Text.Trim();
+                string[] exFilesArray;
+                if (!string.IsNullOrWhiteSpace(exFiles))
+                    exFilesArray = exFiles.ToLower().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                else
+                    exFilesArray = new string[1] { noData };
+
+                //取得檔案存取的時間
+                DateTime mTime = dateTimePicker_Time.Value;
+                DateTime mDate = dateTimePicker_Date.Value.Date.AddHours(mTime.Hour).AddMinutes(mTime.Minute).AddSeconds(mTime.Second);
+                //string mDt = dateTimePicker_Date.Value;
+                //DateTime mDate;
+                //if (!string.IsNullOrWhiteSpace(mDt))
+                //    mDate = DateTime.ParseExact(mDt, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                //else
+                //    mDate = DateTime.ParseExact("1900-01-01 00:00:00", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+                //目標資料夾不存在就產生
+                if (!Directory.Exists(toPath))
+                    Directory.CreateDirectory(toPath);
+
+                //開始找底下包含子目錄所有檔案
+                foreach (string file in Directory.EnumerateFiles(fromPath, "*.*", SearchOption.AllDirectories))
+                {
+                    bool ex = false;
+                    string _file = file.ToLower();
+
+                    //排除的副檔名
+                    foreach (var item in exFilesArray)
+                    {
+                        if (_file.EndsWith(item))
+                        {
+                            ex = true;
+                            break;
+                        }
+                    }
+                    if (ex)
+                        continue;
+
+                    //存取時間
+                    if (File.GetLastWriteTime(file) < mDate)
+                        continue;
+
+                    //檢查路徑上所有資料夾  若有排除的資料夾就跳出
+                    string fromSubPath = Path.GetDirectoryName(file).Replace(fromPath, "").ToLower();
+                    if (!string.IsNullOrWhiteSpace(fromSubPath))
+                    {
+                        string[] splitFromSubPath = fromSubPath.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (splitFromSubPath.Length > 0)
+                            foreach (var item in splitFromSubPath)
+                            {
+                                if (exDirArray.Contains(item))
+                                {
+                                    ex = true;
+                                    break;
+                                }
+                            }
+                    }
+                    if (ex)
+                        continue;
+
+                    //在目的資料夾要架構相同
+                    string toFullPath = file.Replace(fromPath, toPath);
+                    string subFolder = Path.GetDirectoryName(toFullPath);
+                    //DirectoryInfo info = )
+                    //if (exDirArray.Contains(new DirectoryInfo(subFolder).Name.ToLower()))
+                    //    continue;
+
+                    //目標資料夾不存在就產生
+                    if (!Directory.Exists(subFolder))
+                        Directory.CreateDirectory(subFolder);
+
+                    //copy overwrite
+                    File.Copy(file, toFullPath, true);
+                }
+
+                MessageBox.Show("完成！!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
